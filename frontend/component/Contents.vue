7382e99502d3d646a7a9fd5b81b124c816996e50
cioul <icoul@naver.com>
2018-06-03 18:10:09 +0900

    Initialize Mainpage

---
+++ frontend/component/Contents.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="contents">
+        {{ content }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'contents',
+    data() {
+        return {
+            content: '메인페이지입니다'
+        }
+    }
+}
+</script>
