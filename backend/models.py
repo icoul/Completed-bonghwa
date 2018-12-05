ac86c4c133471b8e01dbfff866eb883f9853c8b0
icoul <icoul35@gmail.com>
2018-12-05 21:31:39 +0900

    change Contents Model

--- backend/models.py
+++ backend/models.py
@@ -5,12 +5,12 @@ from django.utils import timezone
 
 # Create your models here.
 class Contents(models.Model):
-    mention_index = models.CharField(max_length=5, default='0')  # 멘션 index
-    mention_order = models.CharField(max_length=5, default='0')  # 멘션 순서
+    mentionIndex  = models.CharField(max_length=5, default='0')  # 멘션 index
+    mentionDepth  = models.CharField(max_length=5, default='0')  # 멘션 순서
     username      = models.CharField(max_length=40)              # 유저 아이디
     contents      = models.CharField(max_length=150)             # 장작 내용
     image         = models.FileField(null=True)                  # 그림
-    created_date  = models.DateTimeField(auto_now=True)          # 작성일
+    createdDate   = models.DateTimeField(auto_now=True)          # 작성일
     dm            = models.IntegerField(default=0)               # DM여부
     deleted       = models.IntegerField(default=0)               # 삭제여부
 
